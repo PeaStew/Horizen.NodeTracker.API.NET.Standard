37af5d816ca9726010d332e1dabde6347b0dfddb
PeaStew <peter.gavin.stewart@gmail.com>
2019-02-03 01:30:02 +0100

    Changing to .NET.Standard

---
+++ Horizen.NodeTracker.API.Net.Standard/APIKey.cs
@@ -0,0 +1,11 @@
+﻿using Horizen.NodeTracker.API.NET.Standard;
+
+namespace Horizen.NodeTracker.API.NET.Standard
+{
+    public class APIKey
+    {
+        public string APIkey { get; set; }
+        public NodeType APINodeType { get; set; }
+    }
+    
+}
